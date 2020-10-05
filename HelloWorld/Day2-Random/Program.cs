df64b7d626c8d1d5d5e89dc9752f1fedbaff2c66
Joshua Michael Savio Fernandes <jf0273@gmail.com>
2020-10-05 14:50:29 +0530

    Random Class

---
+++ HelloWorld/Day2-Random/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day2_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var passLength = 10;
+            var buffer = new char[passLength];
+
+            for (var i = 0; i < passLength; i++) {
+                buffer[i] = (char)('a' + random.Next(0,26));
+            }
+
+            var pass = new string(buffer);
+
+            Console.WriteLine("Password is: {0}", pass);
+        }
+    }
+}
