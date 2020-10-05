a9c1b88397a1887c8e6e0590fa4048cb2671dae1
Joshua Michael Savio Fernandes <jf0273@gmail.com>
2020-10-05 14:21:26 +0530

    Ex 3

---
+++ HelloWorld/Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Landscpae or Portrait!");
+            Console.WriteLine("Enter Width");
+            string width = Console.ReadLine();
+            Console.WriteLine("Enter Height");
+            string height = Console.ReadLine();
+
+            var type = (Convert.ToInt32(width) > Convert.ToInt32(height)) ? "Landscape" : "Portrait"; ;
+
+            Console.WriteLine("Image is of type {0}", type);
+        }
+    }
+}
