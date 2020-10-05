e1a0f2f47cc6612328b34d19d7bf218ccda52a33
Joshua Michael Savio Fernandes <jf0273@gmail.com>
2020-10-05 14:16:43 +0530

    Ex 2

---
+++ HelloWorld/Exercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("CHECK WHICH NUMBER IS GREATER");
+            Console.WriteLine("Enter Num   1");
+            string n1 = Console.ReadLine();
+            Console.WriteLine("Enter Num 2");
+            string n2 = Console.ReadLine();
+
+            if (Convert.ToInt32(n1) > Convert.ToInt32(n2)) Console.WriteLine("{0} is Greated", n1);
+            else Console.WriteLine("{0} is Greater", n2);
+        }
+    }
+}
